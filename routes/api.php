4cb4ac4715c9d8e69d1cde5e684d42a450737706
MOH AH <m.alahmar2000.nole@gmail.com>
2023-07-27 11:09:52 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,5 @@ Route::get('projects/{id}', [ProjectController::class,('show')]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
-
     return $request->user();
 });
