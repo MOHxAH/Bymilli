6bdbf3d37ca2214622b33854a41f63778e9d9719
MOH AH <m.alahmar2000.nole@gmail.com>
2023-08-06 13:59:16 +0300

    edit update method for userprofile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,10 +15,10 @@ class UserController extends Controller
     public function update(Request $request,$id){
 
         $user = User::where('id',$id)->first();
-        $user->full_name = $request->full_name;
-        $user->email = $request->email;
-        $user->phone_number = $request->phone_number;
-        $user->logo = $request->logo;
+        $user->full_name = $request->full_name ?? $user->full_name;
+        $user->email = $request->email ?? $user->email;
+        $user->phone_number = $request->phone_number ?? $user->phone_number;
+        $user->logo = $request->logo ?? $user->logo;
             $user->save();
             //return $request->all();
 
