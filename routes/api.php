c8eed2fa98870cf9cfc992869e52b405f99383ab
MOH AH <m.alahmar2000.nole@gmail.com>
2023-08-13 16:10:11 +0300

    users and project auth

--- routes/api.php
+++ routes/api.php
@@ -23,40 +23,69 @@ use App\Models\Request as ModelsRequest;
 */
 
 // Route::prefix('project')->group(function () {
-//     Route::get('', [ProjectController::class,('view')]);
-//     Route::get('s', [ProjectController::class,('viewAll')]);
-//     Route::post('', [ProjectController::class,('store')]);
-//     Route::get('/{id}', [ProjectController::class,('show')]);
-//     //Route::post('/{id}',[ProjectController::class,('update')]);
+//      Route::get('', [ProjectController::class,('view')]);
+//      Route::get('s', [ProjectController::class,('viewAll')]);
+//      Route::post('', [ProjectController::class,('store')]);
+//      Route::get('/{id}', [ProjectController::class,('show')]);
+// //     //Route::post('/{id}',[ProjectController::class,('update')]);
+// //     //Route::delete('projects/{id}', [TodoController::class, 'destroy']);
+// //     //Route::delete('projects',[ProjectController::class,('destroy')]);
+//  });
+
+// Route::get('project', [ProjectController::class,('view')]);
+// Route::get('projects', [ProjectController::class,('viewAll')]);
+// Route::post('project', [ProjectController::class,('store')]);
+// Route::get('project/{id}', [ProjectController::class,('show')]);
+
+// Route::prefix('users')->group(function () {
+//     //Route::get('', [ProjectController::class,('index')]);
+//     //Route::Post('', [ProjectController::class,('store')]);
+//     Route::post('/{id}',[UserController::class,('update')]);
+//     Route::get('/{id}', [UserController::class,('show')]);
 //     //Route::delete('projects/{id}', [TodoController::class, 'destroy']);
 //     //Route::delete('projects',[ProjectController::class,('destroy')]);
+
 // });
-
-Route::get('project', [ProjectController::class,('view')]);
-Route::get('projects', [ProjectController::class,('viewAll')]);
-Route::post('project', [ProjectController::class,('store')]);
-Route::get('project/{id}', [ProjectController::class,('show')]);
-
-Route::prefix('users')->group(function () {
-    //Route::get('', [ProjectController::class,('index')]);
-    //Route::Post('', [ProjectController::class,('store')]);
-    Route::post('/{id}',[UserController::class,('update')]);
-    Route::get('/{id}', [UserController::class,('show')]);
-    //Route::delete('projects/{id}', [TodoController::class, 'destroy']);
-    //Route::delete('projects',[ProjectController::class,('destroy')]);
-
-});
+//  Route::prefix('project')->group(function () {
+// //     Route::get('', [ProjectController::class,('view')]);
+// //     Route::get('s', [ProjectController::class,('viewAll')]);
+// //     Route::post('', [ProjectController::class,('store')]);
+// //     Route::get('/{id}', [ProjectController::class,('show')]);
+// // //     //Route::post('/{id}',[ProjectController::class,('update')]);
+// // //     //Route::delete('projects/{id}', [TodoController::class, 'destroy']);
+// // //     //Route::delete('projects',[ProjectController::class,('destroy')]);
+//  });
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
-    Route::get('logout', 'logout');
+    //Route::get('logout', 'logout');
     //Route::post('refresh', 'refresh');
 });
-Route::get('email/verify/{id}', 'VerificationController@verify')->name('verification.verify');
-Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
+//Route::get('email/verify/{id}', 'VerificationController@verify')->name('verification.verify');
+//Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
 
 Route::middleware('auth:sanctum')->group(function(){
+    Route::get('logout', [AuthController::class,'logout']);
+    Route::prefix('users')->group(function () {
+        //Route::get('', [ProjectController::class,('index')]);
+        //Route::Post('', [ProjectController::class,('store')]);
+        Route::post('/{id}',[UserController::class,('update')]);
+        Route::get('/{id}', [UserController::class,('show')]);
+        //Route::delete('projects/{id}', [TodoController::class, 'destroy']);
+        //Route::delete('projects',[ProjectController::class,('destroy')]);
+
+    });
+    Route::prefix('project')->group(function () {
+        Route::get('', [ProjectController::class,('view')]);
+        Route::get('s', [ProjectController::class,('viewAll')]);
+        Route::post('', [ProjectController::class,('store')]);
+        Route::get('/{id}', [ProjectController::class,('show')]);
+    //     //Route::post('/{id}',[ProjectController::class,('update')]);
+    //     //Route::delete('projects/{id}', [TodoController::class, 'destroy']);
+    //     //Route::delete('projects',[ProjectController::class,('destroy')]);
+    });
+
 
     Route::get('/projects/{project_id}/eval', [RequestController::class,('viewEval')]);
     Route::get('/projects/{project_id}/uneval',[RequestController::class,('viewUneval')]);
